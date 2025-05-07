f1a1134f6e916b448468f65f975871a3cb01417f
arman-borkhani <armanborkhani@gmail.com>
2025-05-07 20:40:16 +0330

    Fetch genres using react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,22 @@
+import { useQuery } from 'react-query'
+import genres from '../data/genres'
 import Genre from '../entities/Genre'
-import useData from './useData'
+import apiClient from '../services/api-client'
 
 interface FetchGenresResponse {
   genres: Genre[]
 }
 
 const useGenres = () => {
-  const { data, error, isLoading } =
-    useData<FetchGenresResponse>('/genre/movie/list')
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['genres'],
+    queryFn: () =>
+      apiClient
+        .get<FetchGenresResponse>('/genre/movie/list')
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: genres,
+  })
 
   return {
     genres: data?.genres || [],
