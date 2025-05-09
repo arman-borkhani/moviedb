cb8022c255c5804a9ef5901fb22173e1e688bee0
arman-borkhani <armanborkhani@gmail.com>
2025-05-09 11:57:31 +0330

    Implement infinite queries

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,4 +1,5 @@
-import { Alert } from 'antd'
+import { Alert, Button } from 'antd'
+import React from 'react'
 import MovieQuery from '../entities/MovieQuery'
 import useMovies from '../hooks/useMovies'
 import MovieCard from './MovieCard'
@@ -9,25 +10,43 @@ interface Props {
 }
 
 const MovieGrid = ({ movieQuery }: Props) => {
-  const { movies, error, isLoading } = useMovies(movieQuery)
+  const {
+    movies,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useMovies(movieQuery)
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   if (error) return <Alert message={error.message} type="error" />
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-5">
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <div key={skeleton}>
-            <MovieCardSkeleton />
-          </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-5">
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <div key={skeleton}>
+              <MovieCardSkeleton />
+            </div>
+          ))}
+        {movies.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((movie) => (
+              <div key={movie.id}>
+                <MovieCard movie={movie} />
+              </div>
+            ))}
+          </React.Fragment>
         ))}
-      {movies.map((movie) => (
-        <div key={movie.id}>
-          <MovieCard movie={movie} />
-        </div>
-      ))}
-    </div>
+      </div>
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()}>
+          {isFetchingNextPage ? 'Loading...' : 'Load More'}
+        </Button>
+      )}
+    </>
   )
 }
 
--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
 import Movie from '../entities/Movie'
 import MovieQuery from '../entities/MovieQuery'
 import APIClient from '../services/api-client'
@@ -8,27 +8,39 @@ const apiClient = new APIClient<FetchMoviesResponse>('/discover/movie')
 interface FetchMoviesResponse {
   results: Movie[]
   page: number
-  total_pages: number
-  total_results: number
 }
 
 const useMovies = (movieQuery: MovieQuery) => {
-  const { data, error, isLoading } = useQuery<FetchMoviesResponse, Error>({
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<FetchMoviesResponse, Error>({
     queryKey: ['movies', movieQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           with_genres: movieQuery.genre?.id,
           sort_by: movieQuery.sortOrder,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.results.length > 0 ? allPages.length + 1 : undefined
+    },
     staleTime: 24 * 60 * 60 * 1000, // 24h
   })
 
   return {
-    movies: data?.results || [],
+    movies: data?.pages || [],
     error,
     isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
   }
 }
 
