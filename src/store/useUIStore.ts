6c7e098990476f1d6ce125ce2779d10ec0ac29ad
arman-borkhani <armanborkhani@gmail.com>
2025-05-14 21:10:13 +0330

    Persist the darkMode state in local storage

--- src/store/useUIStore.ts
+++ src/store/useUIStore.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface UIStore {
   darkMode: boolean
   toggleDarkMode: () => void
 }
 
-const useUIStore = create<UIStore>((set) => ({
-  darkMode: false,
-  toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
-}))
+const useUIStore = create<UIStore>()(
+  persist(
+    (set) => ({
+      darkMode: false,
+      toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+    }),
+    {
+      name: 'ui-storage',
+      partialize: (state) => ({ darkMode: state.darkMode }),
+    },
+  ),
+)
 
 export default useUIStore
