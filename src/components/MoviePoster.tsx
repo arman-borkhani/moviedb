b22e484ae37a9cdde692bbe75de7934317833088
arman-borkhani <armanborkhani@gmail.com>
2025-08-16 16:49:41 +0330

    Improve image lazy loading in movie poster component

--- src/components/MoviePoster.tsx
+++ src/components/MoviePoster.tsx
@@ -1,5 +1,5 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/blur.css'
+import 'react-lazy-load-image-component/src/effects/opacity.css'
 import noImage from '../assets/no-image-placeholder.svg'
 import Movie from '../entities/Movie'
 import WatchlistRibbon from './WatchlistRibbon'
@@ -9,14 +9,12 @@ interface Props {
 }
 
 const MoviePoster = ({ movie }: Props) => {
-  const placeholderPath = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`
   const path = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
 
   return (
     <figure className="[&_.lazy-load-image-background]:w-full [&_img]:w-full relative">
       <LazyLoadImage
-        effect="blur"
-        placeholderSrc={movie.poster_path ? placeholderPath : undefined}
+        effect="opacity"
         src={movie.poster_path ? path : noImage}
         className="rounded-lg rounded-tl-none min-h-110 object-cover"
       />
